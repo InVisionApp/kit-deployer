c8a5249b4e09ff1d1f09234bc2bc4f294b252f02
Chesley Brown <me@chesleybrown.ca>
2017-04-06 21:16:51 -0230

    fix: ignore events for objects being killed

--- src/lib/health-check.js
+++ src/lib/health-check.js
@@ -24,7 +24,11 @@ class HealthCheck extends EventEmitter {
 	constructor(kubectl, gracePeriod, since) {
 		super();
 		this.events = kubectl.events(since);
-		this.errorTimeoutId = null;
+		this.error = {
+			timeoutId: null,
+			involvedObjectName: null
+		};
+		this.beingKilled = [];
 		this.gracePeriod = (typeof gracePeriod === "undefined") ? 10 * 1000 : gracePeriod * 1000; // 10 seconds
 	}
 
@@ -42,6 +46,22 @@ class HealthCheck extends EventEmitter {
 				return;
 			}
 
+			// Ignore events for involved objects that are being killed
+			if (this.beingKilled.indexOf(event.involvedObject.name) >= 0) {
+				if (this.error.involvedObjectName == event.involvedObject.name) {
+					this.emit("debug", "Clearing healthcheck timeout because " + event.involvedObject.name + " is being killed");
+					clearTimeout(this.error.timeoutId);
+				}
+				return;
+			}
+
+			// If we encounter a Killing event for any involvedObject we should not care about it's health
+			// A "Killing" event is of type "Normal" and means that a pod is purposefully being killed
+			if (event.reason == "Killing") {
+				this.beingKilled.push(event.involvedObject.name);
+				return;
+			}
+
 			// Skip reasons that are excluded from checking
 			if (excludeReasons.indexOf(event.reason) >= 0) {
 				return;
@@ -52,7 +72,8 @@ class HealthCheck extends EventEmitter {
 				if (this.error.timeoutId === null) {
 					// Emit error unless stop is called before grace period expires
 					this.emit("debug", "Healthcheck detected " + event.reason + " error for " + event.involvedObject.name + ", waiting grace period " + this.gracePeriod + "ms before emitting");
-					this.errorTimeoutId = setTimeout(() => {
+					this.error.involvedObjectName = event.involvedObject.name;
+					this.error.timeoutId = setTimeout(() => {
 						this.emit("debug", "Healthcheck grace period of " + this.gracePeriod + "ms expired");
 						this.emit("error", new EventError(event));
 					}, this.gracePeriod);
@@ -65,7 +86,7 @@ class HealthCheck extends EventEmitter {
 	stop() {
 		this.emit("debug", "Stopping healthcheck watcher");
 		this.events.stop();
-		if (this.errorTimeoutId !== null) {
+		if (this.error.timeoutId !== null) {
 			this.emit("debug", "Clearing healthcheck timeout");
 			clearTimeout(this.errorTimeoutId);
 		}
