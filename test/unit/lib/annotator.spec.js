fc1bf39d28ca0300492fab251ea4ccd036fab7a1
Chesley Brown <me@chesleybrown.ca>
2017-05-19 14:31:53 -0230

    feat: Added UUID as annotation

--- test/unit/lib/annotator.spec.js
+++ test/unit/lib/annotator.spec.js
@@ -22,7 +22,8 @@ describe("Annotator", () => {
 			}
 		};
 		const options = {
-			sha: "123abc"
+			uuid: "dafbe5ac-f687-4b19-ba23-8fa91f84fbb8",
+			sha: "e05a1d976d3e5b5b42a4068b0f34be756cbd5f2a"
 		};
 		beforeEach(() => {
 			annotator = new Annotator(options);
@@ -34,6 +35,7 @@ describe("Annotator", () => {
 			it("should set the expected annotations", () => {
 				const manifest = annotator.annotate(_.cloneDeep(originalManifest));
 				expect(manifest.metadata.name).to.equal(originalManifest.metadata.name);
+				expect(manifest.metadata.annotations[Annotations.UUID]).to.equal(options.uuid);
 				expect(manifest.metadata.annotations[Annotations.OriginalName]).to.equal(originalManifest.metadata.name);
 				expect(manifest.metadata.annotations[Annotations.LastAppliedConfiguration]).to.equal(JSON.stringify(originalManifest));
 				expect(manifest.metadata.annotations[Annotations.LastAppliedConfigurationHash]).to.equal("824c6735b631f957a285da2873d1465d797e4e6f");
@@ -44,6 +46,54 @@ describe("Annotator", () => {
 			it("should set the expected annotations", () => {
 				const manifest = annotator.annotate(_.cloneDeep(originalJobManifest));
 				expect(manifest.metadata.name).to.equal(originalJobManifest.metadata.name + "-f94274f6bdc905825d1616fe265bc6d2de773e7c");
+				expect(manifest.metadata.annotations[Annotations.UUID]).to.equal(options.uuid);
+				expect(manifest.metadata.annotations[Annotations.OriginalName]).to.equal(originalJobManifest.metadata.name);
+				expect(manifest.metadata.annotations[Annotations.LastAppliedConfiguration]).to.equal(JSON.stringify(originalJobManifest));
+				expect(manifest.metadata.annotations[Annotations.LastAppliedConfigurationHash]).to.equal("f94274f6bdc905825d1616fe265bc6d2de773e7c");
+				expect(manifest.metadata.annotations[Annotations.Commit]).to.equal(JSON.stringify(options.sha));
+			});
+		});
+	});
+
+	describe("Create New without UUID", () => {
+		let annotator;
+		const originalManifest = {
+			kind: "Deployment",
+			metadata: {
+				name: "manifest-deployment"
+			}
+		};
+		const originalJobManifest = {
+			kind: "Job",
+			metadata: {
+				name: "manifest-job"
+			}
+		};
+		const options = {
+			sha: "e05a1d976d3e5b5b42a4068b0f34be756cbd5f2a"
+		};
+		beforeEach(() => {
+			annotator = new Annotator(options);
+		});
+		it("should be cool with it", () => {
+			expect(annotator.options.sha).to.equal(options.sha);
+		});
+		describe("and calling annotate on deployment", () => {
+			it("should set the expected annotations", () => {
+				const manifest = annotator.annotate(_.cloneDeep(originalManifest));
+				expect(manifest.metadata.name).to.equal(originalManifest.metadata.name);
+				expect(manifest.metadata.annotations[Annotations.UUID]).to.be.empty;
+				expect(manifest.metadata.annotations[Annotations.OriginalName]).to.equal(originalManifest.metadata.name);
+				expect(manifest.metadata.annotations[Annotations.LastAppliedConfiguration]).to.equal(JSON.stringify(originalManifest));
+				expect(manifest.metadata.annotations[Annotations.LastAppliedConfigurationHash]).to.equal("824c6735b631f957a285da2873d1465d797e4e6f");
+				expect(manifest.metadata.annotations[Annotations.Commit]).to.equal(JSON.stringify(options.sha));
+			});
+		});
+		describe("and calling annotate on job", () => {
+			it("should set the expected annotations", () => {
+				const manifest = annotator.annotate(_.cloneDeep(originalJobManifest));
+				expect(manifest.metadata.name).to.equal(originalJobManifest.metadata.name + "-f94274f6bdc905825d1616fe265bc6d2de773e7c");
+				expect(manifest.metadata.annotations[Annotations.UUID]).to.be.empty;
 				expect(manifest.metadata.annotations[Annotations.OriginalName]).to.equal(originalJobManifest.metadata.name);
 				expect(manifest.metadata.annotations[Annotations.LastAppliedConfiguration]).to.equal(JSON.stringify(originalJobManifest));
 				expect(manifest.metadata.annotations[Annotations.LastAppliedConfigurationHash]).to.equal("f94274f6bdc905825d1616fe265bc6d2de773e7c");
