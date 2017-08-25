9e00b139522e3c54c9dfaeef001238e4ed9c97f5
Chesley Brown <me@chesleybrown.ca>
2017-08-25 12:32:00 -0230

    Apply prettier to scripts js too

--- scripts/download-kubectl.js
+++ scripts/download-kubectl.js
@@ -12,15 +12,22 @@ var kubectlFile = fs.createWriteStream(kubectlPath);
 
 // Make the kubectl executable
 kubectlFile
-	.on("error", function(err) {
-		throw err;
-	})
-	.on("finish", function() {
-		fs.chmodSync(kubectlPath, "770");
-	});
+  .on("error", function(err) {
+    throw err;
+  })
+  .on("finish", function() {
+    fs.chmodSync(kubectlPath, "770");
+  });
 
-https.get("https://storage.googleapis.com/kubernetes-release/release/" + process.env.KUBE_VERSION + "/bin/linux/amd64/kubectl", function(response) {
-	response.pipe(kubectlFile);
-}).on("error", function(err) {
-	throw err;
-});
+https
+  .get(
+    "https://storage.googleapis.com/kubernetes-release/release/" +
+      process.env.KUBE_VERSION +
+      "/bin/linux/amd64/kubectl",
+    function(response) {
+      response.pipe(kubectlFile);
+    }
+  )
+  .on("error", function(err) {
+    throw err;
+  });
